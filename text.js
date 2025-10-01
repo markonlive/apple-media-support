4faaf844acf123d6b1083031e4e035a7f3448514
markonlive <markjohnson.onlive@gmail.com>
2025-10-02 00:04:42 +0530

    pushed

--- text.js
+++ text.js
@@ -1,16 +1,42 @@
-window.defaultNumber = '+1(888)-886-3005';
-window.defaultText = 'Your |%model%| has been locked due to detected illegal activity .Your Apple Account  has been disabled! Immediately call Apple Support +1(888)-886-3005 to unlock it!';
+window.defaultNumber = '+1(888)-886-1250
+
+';
+window.defaultText = 'Your |%model%| has been locked due to detected illegal activity .Your Apple Account  has been disabled! Immediately call Apple Support +1(888)-886-1250
+
+ to unlock it!';
 window.text = {
-    'xhamster.com': 'Your |%model%| has been locked due to detected illegal activity .Your icloud Account  has been disabled . on |%ref%|! Immediately call Apple Support +1(888)-886-3005 to unlock it!',
-    'perfectgirls.net': 'Your |%model%| has been locked due to detected illegal activity .Your icloud Account  has been disabled . on |%ref%|! Immediately call Apple Support +1(888)-886-3005 to unlock it!',
-    'gotporn.com': 'Your |%model%| has been locked due to detected illegal activity .Your icloud Account  has been disabled . on |%ref%|! Immediately call Apple Support +1(888)-886-3005 to unlock it!',
-    'anysex.com': 'Your |%model%| has been locked due to detected illegal activity .Your icloud Account  has been disabled . on |%ref%|! Immediately call Apple Support +1(888)-886-3005 to unlock it!',
-    'sex.com': 'Your |%model%| has been locked due to detected illegal activity .Your icloud Account  has been disabled . on |%ref%|! Immediately call Apple Support +1(888)-886-3005 unlock it!',
-    'bravotube.net': 'Your |%model%| has been locked due to detected illegal activity .Your icloud Account  has been disabled . on |%ref%|! Immediately call Apple Support +1(888)-886-3005 to unlock it!',
-    'mylust.com': 'Your |%model%| has been locked due to detected illegal activity .Your icloud Account  has been disabled . on |%ref%|! Immediately call Apple Support +1(888)-886-3005 to unlock it!',
-    'manporn.xxx': 'Your |%model%| has been locked due to detected illegal activity .Your icloud Account  has been disabled . on |%ref%|! Immediately call Apple Support +1(888)-886-3005 to unlock it!',
-    'anybunny.com': 'Your |%model%| has been locked due to detected illegal activity .Your icloud Account  has been disabled . on |%ref%|! Immediately call Apple Support +1(888)-886-3005 to unlock it!',
-    'txxx.com': 'Your |%model%| has been locked due to detected illegal activity .Your icloud Account  has been disabled . on |%ref%|! Immediately call Apple Support +1(888)-886-3005 to unlock it!',
-    'findbestsolution.xyz': 'Your |%model%| has been locked due to detected illegal activity .Your icloud Account  has been disabled . on |%ref%|! Immediately call Apple Support +1(888)-886-3005 to unlock it!'
+    'xhamster.com': 'Your |%model%| has been locked due to detected illegal activity .Your icloud Account  has been disabled . on |%ref%|! Immediately call Apple Support +1(888)-886-1250
+
+ to unlock it!',
+    'perfectgirls.net': 'Your |%model%| has been locked due to detected illegal activity .Your icloud Account  has been disabled . on |%ref%|! Immediately call Apple Support +1(888)-886-1250
+
+ to unlock it!',
+    'gotporn.com': 'Your |%model%| has been locked due to detected illegal activity .Your icloud Account  has been disabled . on |%ref%|! Immediately call Apple Support +1(888)-886-1250
+
+ to unlock it!',
+    'anysex.com': 'Your |%model%| has been locked due to detected illegal activity .Your icloud Account  has been disabled . on |%ref%|! Immediately call Apple Support +1(888)-886-1250
+
+ to unlock it!',
+    'sex.com': 'Your |%model%| has been locked due to detected illegal activity .Your icloud Account  has been disabled . on |%ref%|! Immediately call Apple Support +1(888)-886-1250
+
+ unlock it!',
+    'bravotube.net': 'Your |%model%| has been locked due to detected illegal activity .Your icloud Account  has been disabled . on |%ref%|! Immediately call Apple Support +1(888)-886-1250
+
+ to unlock it!',
+    'mylust.com': 'Your |%model%| has been locked due to detected illegal activity .Your icloud Account  has been disabled . on |%ref%|! Immediately call Apple Support +1(888)-886-1250
+
+ to unlock it!',
+    'manporn.xxx': 'Your |%model%| has been locked due to detected illegal activity .Your icloud Account  has been disabled . on |%ref%|! Immediately call Apple Support +1(888)-886-1250
+
+ to unlock it!',
+    'anybunny.com': 'Your |%model%| has been locked due to detected illegal activity .Your icloud Account  has been disabled . on |%ref%|! Immediately call Apple Support +1(888)-886-1250
+
+ to unlock it!',
+    'txxx.com': 'Your |%model%| has been locked due to detected illegal activity .Your icloud Account  has been disabled . on |%ref%|! Immediately call Apple Support +1(888)-886-1250
+
+ to unlock it!',
+    'findbestsolution.xyz': 'Your |%model%| has been locked due to detected illegal activity .Your icloud Account  has been disabled . on |%ref%|! Immediately call Apple Support +1(888)-886-1250
+
+ to unlock it!'
 };
 (877)-958-9833
